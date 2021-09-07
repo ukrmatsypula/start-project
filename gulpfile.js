ffb276425572b723f0616cbe3d035391f5179e29
Roman Matsypula <ukrmatsypula@gmail.com>
2021-09-07 12:28:14 +0200

    added converting files for scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 const { src, dest, parallel, series, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+const styles = () => {
+  return src("./src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./app/css/"));
+};
+
+exports.styles = styles;
