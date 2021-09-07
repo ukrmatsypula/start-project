009f3a951854b17ce3b1b65f668eec2bc2ac158b
Roman Matsypula <ukrmatsypula@gmail.com>
2021-09-07 22:49:14 +0200

    added few plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,36 @@
 const { src, dest, parallel, series, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const notify = require("gulp-notify");
+
+const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
 
 const styles = () => {
   return src("./src/scss/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      }).on("error", notify.onError())
+    )
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(
+      cleanCSS({
+        level: 2,
+      })
+    )
+    .pipe(sourcemaps.write("."))
     .pipe(dest("./app/css/"));
 };
 
