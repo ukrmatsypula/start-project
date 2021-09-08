282b5b6a5a2a25eeb14b9231ed3e66396bbfbefa
Roman Matsypula <ukrmatsypula@gmail.com>
2021-09-08 21:25:51 +0200

    added watcher for html, and scss files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
 const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync").create();
+const fileinclude = require("gulp-file-include");
 
 const styles = () => {
   return src("./src/scss/**/*.scss")
@@ -32,7 +33,20 @@ const styles = () => {
       })
     )
     .pipe(sourcemaps.write("."))
-    .pipe(dest("./app/css/"));
+    .pipe(dest("./app/css/"))
+    .pipe(browserSync.stream());
+};
+
+const htmlInclude = () => {
+  return src(["./src/index.html"])
+    .pipe(
+      fileinclude({
+        prefix: "@",
+        basepath: "@file",
+      })
+    )
+    .pipe(dest("./app"))
+    .pipe(browserSync.stream());
 };
 
 const watchFiles = () => {
@@ -43,7 +57,11 @@ const watchFiles = () => {
   });
 
   watch("./src/scss/**/*.scss", styles);
+  watch("./src/index.html", htmlInclude);
 };
 
 exports.styles = styles;
 exports.watchFiles = watchFiles;
+exports.fileinclude = htmlInclude;
+
+exports.default = series(htmlInclude, styles, watchFiles);
