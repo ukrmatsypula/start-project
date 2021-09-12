5fab6e6bb556fe7a9d3cd5c30ecb7e682bcfe844
Roman Matsypula <ukrmatsypula@gmail.com>
2021-09-12 23:36:09 +0200

    added new structure and task for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,25 @@
-const { src, dest, parallel, series, watch } = require("gulp");
+const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const notify = require("gulp-notify");
-
-const rename = require("gulp-rename");
-const autoprefixer = require("gulp-autoprefixer");
-const cleanCSS = require("gulp-clean-css");
-const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync").create();
-const fileinclude = require("gulp-file-include");
-const svgSprite = require("gulp-svg-sprite");
-const ttf2woff = require("gulp-ttf2woff");
-const ttf2woff2 = require("gulp-ttf2woff2");
-const del = require("del");
 
-const fonts = () => {
-  src("./src/fonts/**.ttf").pipe(ttf2woff()).pipe(dest("./app/fonts/"));
-  return src("./src/fonts/**.ttf").pipe(ttf2woff2()).pipe(dest("./app/fonts"));
-};
-
-const svgSprites = () => {
-  return src("./src/img/**.svg")
-    .pipe(
-      svgSprite({
-        mode: {
-          stack: {
-            sprite: "../sprite.svg",
-          },
-        },
-      })
-    )
-    .pipe(dest("./app/img"));
-};
-
-const styles = () => {
-  return src("./src/scss/**/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        outputStyle: "expanded",
-      }).on("error", notify.onError())
-    )
-    .pipe(
-      rename({
-        suffix: ".min",
-      })
-    )
-    .pipe(
-      autoprefixer({
-        cascade: false,
-      })
-    )
-    .pipe(
-      cleanCSS({
-        level: 2,
-      })
-    )
-    .pipe(sourcemaps.write("."))
-    .pipe(dest("./app/css/"))
+function style() {
+  return gulp
+    .src("./src/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream());
-};
+}
 
-const htmlInclude = () => {
-  return src(["./src/index.html"])
-    .pipe(
-      fileinclude({
-        prefix: "@",
-        basepath: "@file",
-      })
-    )
-    .pipe(dest("./app"))
-    .pipe(browserSync.stream());
-};
-
-const imgToApp = () => {
-  return src([
-    "./src/img/**.jpg",
-    "./src/img/**.png",
-    "./src/img/**.jpeg",
-  ]).pipe(dest("./app/img"));
-};
-
-const resources = () => {
-  return src("./src/resources/**").pipe(dest("./app"));
-};
-
-const watchFiles = () => {
+function watch() {
   browserSync.init({
     server: {
-      baseDir: "./app",
+      baseDir: "./src",
     },
   });
 
-  const clean = () => {
-    return del(["app/**"]);
-  };
+  gulp.watch("./src/scss/**/*.scss", style);
+  gulp.watch("./src/*.html").on("change", browserSync.reload);
+}
 
-  watch("./src/scss/**/*.scss", styles);
-  watch("./src/index.html", htmlInclude);
-  watch("./src/img/**.jpg", imgToApp);
-  watch("./src/img/**.jpeg", imgToApp);
-  watch("./src/img/**.png", imgToApp);
-  watch("./src/img/**.svg", svgSprites);
-  watch("./src/resources/**", resources);
-  watch("./src/fonts/**.ttf", fonts);
-};
-
-exports.styles = styles;
-exports.watchFiles = watchFiles;
-exports.fileinclude = htmlInclude;
-
-exports.default = series(
-  clean,
-  parallel(htmlInclude, fonts, resources, imgToApp, svgSprites),
-  styles,
-  watchFiles
-);
+exports.style = style;
+exports.watch = watch;
